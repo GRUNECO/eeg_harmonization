d899bafe34579c3cb7eb93f49a32dbf0e67ea722
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2022-02-24 15:14:57 -0500

    separate by task

--- analysis.py
+++ analysis.py
@@ -2,25 +2,39 @@ import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_excel(r"C:\Users\user\Desktop\longitudinal_data_icpowers_avengers.xlsx",sheet_name="Sheet1")
 
-all_columns = list(df.columns)
-#derivatives = [x if '_r' in x for x in all_columns]
-BANDS = {"Delta","Theta","Alpha-1","Alpha-2","Alpha","Beta","Gamma"}
-GROUPS = {'G1', 'DCL', 'DTA', 'G2', 'CTR'}
-TASKS = {'CE','OE'}
+ALL_COLUMNS = list(df.columns)
+DERIVATIVES = set([x  for x in ALL_COLUMNS if '_r' in x])
+VARIABLES = set([x  for x in ALL_COLUMNS if '_r' not in x and 'Unnamed' not in x])
+BANDS = set([x.split('_r')[1] for x in DERIVATIVES])
+GROUPS = set(df.group)
+TASKS = set(df.condition)
+SESSIONS = set(df.visit)
+
+for v in VARIABLES:
+    print('*'*100)
+    print(v)
+    print(set(df[v]))
+
+print('*'*100)
+print('BANDS')
+print(BANDS)
+
 # Transversal Analysis
-task = 
+task = 'CE'
 component = 15
 visit = 'V1'
 band = 'Alpha-2'
 dft = df[df['visit']==visit]
-
+dft = dft[dft['condition']==task]
 dft.boxplot(column=['C'+str(component)+'_r'+band] ,by='group')
 plt.show()
 
 # Longitudinal Analysis
 component = 15
 band = 'Alpha-2'
-dft = df[df['group']==group]
 group = 'DCL'
-dft.boxplot(column=['C'+str(component)+'_r'+band] ,by='visit')
+
+dfl = df[df['group']==group]
+dfl = dfl[dfl['condition']==task]
+dfl.boxplot(column=['C'+str(component)+'_r'+band] ,by='visit')
 plt.show()

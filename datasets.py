bbed8cf84f180e849abe2bbb67ba1f01d64dc7c4
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2022-04-10 16:44:57 -0500

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -2,35 +2,30 @@ BIOMARCADORES = {
     'name':'BIOMARCADORES',
     'input_path':r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS',
     'layout':{'extension':'.vhdr', 'task':'OE','suffix':'eeg', 'return_type':'filename'},
-    'args':{'line_freqs':[60]},
+    'args':{'line_freqs':[60],'events_to_keep':None},
     'group_regex':'(.+).{3}',
-    'marks_rest':None,
 }
 
 SRM = {
     'name':'SRM',
     'input_path':r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\SRM',
     'layout':{'extension':'.edf', 'task':'resteyesc','suffix':'eeg', 'return_type':'filename'},
-    'args':{'line_freqs':[50]},
+    'args':{'line_freqs':[50],'events_to_keep':None},
     'group_regex':None,
-    'marks_rest':None,
 }
 
 CHBMP = {
     'name':'CHBMP',
     'input_path':r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\CHBMP',
     'layout':{'extension':'.edf', 'task':'protmap','suffix':'eeg', 'return_type':'filename'},
-    'args':{'line_freqs':[60]},
+    'args':{'line_freqs':[60],'events_to_keep':[65]},
     'group_regex':None,
-    'marks_rest':65,
 }
 
 LEMON = {
     'name':'LEMON',
     'input_path':r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\LEMON_BIDS',
     'layout':{'extension':'.vhdr', 'task':'resting','suffix':'eeg', 'return_type':'filename'},
-    'args':{'resample':1000,'line_freqs':[50]},
+    'args':{'resample':1000,'line_freqs':[50],'events_to_keep':[5]},
     'group_regex':None,
-    'marks_rest':5,
-
 }

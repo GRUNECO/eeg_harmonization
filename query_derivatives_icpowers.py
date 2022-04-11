326f9e1ccdf949ab9e02a2804ed5a36c4503d4fb
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2022-04-10 20:17:23 -0500

    Update query_derivatives_icpowers.py

--- query_derivatives_icpowers.py
+++ query_derivatives_icpowers.py
@@ -11,6 +11,7 @@ input_path = THE_DATASET.get('input_path',None)
 task = THE_DATASET.get('layout',None).get('task',None)
 group_regex = THE_DATASET.get('group_regex',None)
 name = THE_DATASET.get('name',None)
+runlabel = THE_DATASET.get('run-label','')
 
 data_path = input_path
 layout = BIDSLayout(data_path,derivatives=True)
@@ -18,7 +19,7 @@ layout.get(scope='derivatives', return_type='file')
 
 
 
-eegs_powers = layout.get(extension='.txt',task=task,suffix='icpowers', return_type='filename')
+eegs_powers = layout.get(extension='.txt',task=task,desc ='component'+f'[{runlabel}]',suffix='powers', return_type='filename')
 
 list_subjects = []
 for i in range(len(eegs_powers)):

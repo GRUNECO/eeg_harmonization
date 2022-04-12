8fd75fc4c01e92791d57410f9b17fd851b237bfa
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2022-04-12 18:44:16 -0500

    convert harmonization to a package

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+import versioneer
+
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
+
+# Give setuptools a hint to complain if it's too old a version
+# 30.3.0 allows us to put most metadata in setup.cfg
+# 38.3.0 contains most setup.cfg bugfixes
+# Should match pyproject.toml
+SETUP_REQUIRES = ["setuptools >= 38.3.0"]
+
+
+setuptools.setup(
+    setup_requires=SETUP_REQUIRES,
+    install_requires = REQUIREMENTS,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
---
+++ sovaharmony/info.py
@@ -0,0 +1,5 @@
+info = {
+    'Description': "sovaharmony, a harmonization eeg pipeline using the bids standard",
+    'Name':'sovaharmony',
+    'CodeURL':'https://github.com/GRUNECO/eeg-harmonization',
+}

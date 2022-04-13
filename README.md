60894ff4253bb9ab0ddca57a1bdd82cabc3dbca6
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2022-04-12 19:31:02 -0500

    add example and bug fix

--- README.md
+++ README.md
@@ -20,3 +20,22 @@ Then in the eeg-harmonization repo folder
 git pull
 pip install -r requirements-install.txt
 ```
+
+## Usage
+
+```python
+
+from sovaharmony.processing import harmonize
+
+BIOMARCADORES = {
+    'name':'BIOMARCADORES',
+    'input_path':'E:/Datos/BASESDEDATOS/BIOMARCADORES_BIDS',
+    'layout':{'extension':'.vhdr', 'task':'OE','suffix':'eeg', 'return_type':'filename'},
+    'args':{'line_freqs':[60]},
+    'group_regex':'(.+).{3}',
+    'events_to_keep':None,
+    'run-label':'restEC'
+}
+
+harmonize(BIOMARCADORES)
+```
--- sovaharmony/processing.py
+++ sovaharmony/processing.py
@@ -52,7 +52,7 @@ def harmonize(THE_DATASET,fast_mode=False):
     Example:
     THE_DATASET = {
     'name':'BIOMARCADORES',
-    'input_path':r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS',
+    'input_path':'E:/Datos/BASESDEDATOS/BIOMARCADORES_BIDS',
     'layout':{'extension':'.vhdr', 'task':'OE','suffix':'eeg', 'return_type':'filename'},
     'args':{'line_freqs':[60]},
     'group_regex':'(.+).{3}',
@@ -61,7 +61,12 @@ def harmonize(THE_DATASET,fast_mode=False):
     'channels':['FP1', 'FPZ', 'FP2', 'AF3', 'AF4', 'F7', 'F5', 'F3', 'F1', 'FZ', 'F2', 'F4', 'F6', 'F8', 'FC5', 'FC3', 'FC1', 'FCZ', 'FC2', 'FC4', 'FC6', 'T7', 'C5', 'C3', 'C1', 'CZ', 'C2', 'C4', 'C6', 'T8', 'TP7', 'CP5', 'CP3', 'CP1', 'CPZ', 'CP2', 'CP4', 'CP6', 'TP8', 'P7', 'P5', 'P3', 'P1', 'PZ', 'P2', 'P4', 'P6', 'P8', 'PO7', 'PO5', 'PO3', 'POZ', 'PO4', 'PO6', 'PO8', 'O1', 'OZ', 'O2']
     'spatial_filter':'58x25',
     }
+
+    Output:
+
+    list of str, filepaths of files with errors
     """
+
     # Dataset dependent inputs
     input_path = THE_DATASET.get('input_path',None)
     default_channels = ['FP1', 'FPZ', 'FP2', 'AF3', 'AF4', 'F7', 'F5', 'F3', 'F1', 'FZ', 'F2', 'F4', 'F6', 'F8', 'FC5', 'FC3', 'FC1', 'FCZ', 'FC2', 'FC4', 'FC6', 'T7', 'C5', 'C3', 'C1', 'CZ', 'C2', 'C4', 'C6', 'T8', 'TP7', 'CP5', 'CP3', 'CP1', 'CPZ', 'CP2', 'CP4', 'CP6', 'TP8', 'P7', 'P5', 'P3', 'P1', 'PZ', 'P2', 'P4', 'P6', 'P8', 'PO7', 'PO5', 'PO3', 'POZ', 'PO4', 'PO6', 'PO8', 'O1', 'OZ', 'O2']

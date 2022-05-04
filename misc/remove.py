d36ec69236edf49793279a124213a7e728c05c7f
vhenaoi <vhenao.isaza@gmail.com>
2022-05-04 15:24:41 -0500

    remove

--- misc/remove.py
+++ misc/remove.py
@@ -4,19 +4,21 @@ import numpy as np
 
 import requests
   
-#subjects = os.listdir(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaflow')
-subjects = os.listdir(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\SRM')
+subjects = os.listdir(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony')
+#subjects = os.listdir(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\SRM')
 json_remove = []
 for sub in subjects:
     suffix = "sub"
     if sub.endswith(suffix,0,3) == True:
-        #ses = os.listdir(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaflow'+'/'+sub)
-        ses = os.listdir(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\SRM'+'/'+sub)
+        ses = os.listdir(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony'+'/'+sub)
+        #ses = os.listdir(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\SRM'+'/'+sub)
         for v in ses:
-            #icpowers_json_files = glob.glob(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaflow'+'/'+sub+'/'+v+'\eeg/*_icpowers.txt')
-            icpowers_json_files = glob.glob(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\SRM'+'/'+sub+'/'+v+'\eeg/*_eeg.edf')
+            icpowers_json_files = glob.glob(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony'+'/'+sub+'/'+v+'\eeg/*]_stats.txt')
+            #icpowers_json_files = glob.glob(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\SRM'+'/'+sub+'/'+v+'\eeg/*_eeg.edf')
             json_remove.append(icpowers_json_files)
-            
+print(json_remove)
+
+       
 for i in json_remove:
      for j in range(0,2):
          try:
@@ -24,3 +26,4 @@ for i in json_remove:
              os.remove(i[j])
          except:
           continue 
+

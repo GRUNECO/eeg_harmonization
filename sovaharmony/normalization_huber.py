b844ba026c5b96961e8dfe816e4a29e198c0f871
vhenaoi <vhenao.isaza@gmail.com>
2022-05-06 15:28:32 -0500

    removeynorm

--- sovaharmony/normalization_huber.py
+++ sovaharmony/normalization_huber.py
@@ -32,6 +32,7 @@ for i,eeg_file in enumerate(eegs):
     json_dict["Sources"]=norm_path.replace(bids_root,'')
     reject_path = get_derivative_path(layout,eeg_file,'reject'+pipelabel,'eeg','.fif',bids_root,derivatives_root)
     signal = mne.read_epochs(reject_path)
+    signal2=signal.copy()
     signal_hp = signal.filter(None,20,fir_design='firwin')
     (e, c, t) = signal_hp._data.shape
     da_eeg_cont = np.reshape(signal_hp,(c,e*t),order='F')
@@ -41,37 +42,12 @@ for i,eeg_file in enumerate(eegs):
         std_ch.append(mad_std(ch))
     
     k = sm.robust.scale.huber(np.array(std_ch))
-    signal_norm = signal._data/k[0]
-    
-    signal_nn = np.resize(signal_norm,(c,e,t))
-    (c, e, t) = signal_nn.shape
-    signal_norm = np.reshape(signal_nn,(c,e*t),order='F')
-    signal_norma = createRaw(signal_norm,signal.info['sfreq'],ch_names=signal.info['ch_names'])
-    
-    
-    if THE_DATASET.get('events_to_keep', None) is not None:
-        events_file = os.path.splitext(eeg_file)[0].replace('_eeg','_events.tsv')
-        events_raw=pd.read_csv(events_file,sep='\t')
-        samples = events_raw['sample'].tolist()
-        values = events_raw['value'].tolist()
-        events = list(zip(values,samples))
-        events_to_keep = THE_DATASET.get('events_to_keep', None)
-    else:
-        events = None
-        events_to_keep = None
-    
-    signal_n = crop_raw_data(signal_norma,events, events_to_keep)
-    raw_norm = make_fixed_length_epochs(signal_n,duration=e/signal.info['sfreq'],reject_by_annotation=False,preload=True) 
-    raw_norm.save(norm_path ,split_naming='bids', overwrite=True)
+    signal2._data=signal._data/k[0]
+    signal2.save(norm_path ,split_naming='bids', overwrite=True)
     write_json(json_dict,norm_path.replace('.fif','.json'))
 
     #signal_n = mne.io.read_raw(norm_path,preload=True)
     signal_normas = mne.read_epochs(norm_path)
-    signal_up = np.resize(signal_normas,(c,e,t))
-    (c, e, t) = signal_up.shape
-    signal_norm_up = np.reshape(signal_up,(c,e*t),order='F')
-    signal_norma_up = createRaw(signal_norm_up,signal.info['sfreq'],ch_names=signal.info['ch_names'])
-
     power_norm = get_power_derivates(signal_normas)
     
     

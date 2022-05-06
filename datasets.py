87a8e90906d699a517b01f70ddbb99bda2098f86
vhenaoi <vhenao.isaza@gmail.com>
2022-05-05 21:07:07 -0500

    remove

---
+++ datasets.py
@@ -0,0 +1,59 @@
+BIOMARCADORES = {
+    'name':'BIOMARCADORES',
+    'input_path':r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS',
+    'layout':{'extension':'.vhdr', 'task':'CE','suffix':'eeg', 'return_type':'filename'},
+    'args':{'line_freqs':[60]},
+    'group_regex':'(.+).{3}',
+    'events_to_keep':None,
+    'run-label':'restCE'
+}
+
+BIOMARCADORES_test = {
+    'name':'BIOMARCADORES',
+    'input_path':r'D:\WEB\backend\filesSaved\PRUEBA',
+    'layout':{'extension':'.vhdr', 'task':'CE','suffix':'eeg', 'return_type':'filename'},
+    'args':{'line_freqs':[60]},
+    'group_regex':'(.+).{3}',
+    'events_to_keep':None,
+    'run-label':'restCE'
+}
+
+SRM_test = {
+    'name':'SRM',
+    #'input_path':r'D:\WEB\backend\filesSaved\SRMPrueba',
+    'input_path':r'D:\WEB\backend\filesSaved\SRM_02',
+    'layout':{'extension':'.edf', 'task':'resteyesc','suffix':'eeg', 'return_type':'filename'},
+    'args':{'line_freqs':[50]},
+    'group_regex':None,
+    'events_to_keep':None,
+    'run-label':'restCE'
+}
+
+SRM= {
+    'name':'SRM',
+    'input_path':r'D:\WEB\backend\filesSaved\SRM',
+    'layout':{'extension':'.edf', 'task':'resteyesc','suffix':'eeg', 'return_type':'filename'},
+    'args':{'line_freqs':[50]},
+    'group_regex':None,
+    'events_to_keep':None,
+    'run-label':'restCE'
+}
+# CHBMP = {
+#     'name':'CHBMP',
+#     'input_path':r'D:\WEB\backend\filesSaved\CHBMP',
+#     'layout':{'extension':'.edf', 'task':'protmap','suffix':'eeg', 'return_type':'filename'},
+#     'args':{'line_freqs':[60],},
+#     'group_regex':None,
+#     'events_to_keep':[65],
+#     'run-label':'restCE'
+# }
+
+# LEMON = {
+#     'name':'LEMON',
+#     'input_path':r'D:\WEB\backend\filesSaved\LEMON_BIDS',
+#     'layout':{'extension':'.vhdr', 'task':'resting','suffix':'eeg', 'return_type':'filename'},
+#     'args':{'resample':1000,'line_freqs':[50],},
+#     'group_regex':None,
+#     'events_to_keep':[5],
+#     'run-label':'restCE'
+# }
--- misc/remove.py
+++ misc/remove.py
@@ -13,17 +13,32 @@ for sub in subjects:
         ses = os.listdir(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony'+'/'+sub)
         #ses = os.listdir(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\SRM'+'/'+sub)
         for v in ses:
-            icpowers_json_files = glob.glob(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony'+'/'+sub+'/'+v+'\eeg/-reject*')
             #icpowers_json_files = glob.glob(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\SRM'+'/'+sub+'/'+v+'\eeg/*_eeg.edf')
+            # icpowers_json_files = glob.glob(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony'+'/'+sub+'/'+v+'\eeg/*]_eeg.fif')
+            # json_remove.append(icpowers_json_files)
+            # icpowers_json_files = glob.glob(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony'+'/'+sub+'/'+v+'\eeg/*]_eeg.json')
+            # json_remove.append(icpowers_json_files)
+            # icpowers_json_files = glob.glob(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony'+'/'+sub+'/'+v+'\eeg/*]_stats.txt')
+            # json_remove.append(icpowers_json_files)
+            # icpowers_json_files = glob.glob(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony'+'/'+sub+'/'+v+'\eeg/*]_stats.json')
+            # json_remove.append(icpowers_json_files)
+
+            icpowers_json_files = glob.glob(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony'+'/'+sub+'/'+v+'\eeg/*]_powers.txt')
+            json_remove.append(icpowers_json_files)
+            icpowers_json_files = glob.glob(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony'+'/'+sub+'/'+v+'\eeg/*]_powers.json')
+            json_remove.append(icpowers_json_files)
+            icpowers_json_files = glob.glob(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony'+'/'+sub+'/'+v+'\eeg/*]_powers.json')
+            json_remove.append(icpowers_json_files)
+            icpowers_json_files = glob.glob(r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony'+'/'+sub+'/'+v+'\eeg/*]_powers.txt')
             json_remove.append(icpowers_json_files)
 print(json_remove)
 
-       
-'''for i in json_remove:
+    
+for i in json_remove:
      for j in range(0,2):
          try:
              print(i[j])
              os.remove(i[j])
          except:
-          continue '''
+          continue 
 

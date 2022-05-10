f7bd5167c10f54f8443da09275b77b6246567763
vhenaoi <vhenao.isaza@gmail.com>
2022-05-10 17:45:39 -0500

    para los EC

--- misc/remove.py
+++ misc/remove.py
@@ -1,7 +1,8 @@
 import os
 import glob
 
-name = ['norm_eeg','norm_powers','powers_norm']
+name1 = ['_eeg','_stats']
+name2 = ['[restEC]']
 ext = ['.fif','.json','.txt']
 path = None
 #path = r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony'
@@ -10,10 +11,11 @@ path = None
 #path = r'D:\WEB\backend\filesSaved\SRM\derivatives\sovaharmony'
 def remove_data(path,sub,v,name,ext):
 #def remove_data(path,sub,name,ext):
-    path_def = path+'\\'+sub+'\\'+v+'\\eeg\\*'+name+ext
+    path_def = path+'\\'+sub+'\\'+v+'\\eeg\\*'+name2+name1+ext
+    path_d = path+'\\'+sub+'\\'+v+'\\eeg\\*'+name1+ext
     #path_def = path+'\\'+sub+'\\eeg\\*'+name+ext
-    if name in path_def:
-        icpowers_json_files = glob.glob(path_def)
+    if name2 in path_def:
+        icpowers_json_files = glob.glob(path_d)
         #icpowers_json_files = glob.glob(path_def)
         json_remove.append(icpowers_json_files)
     return json_remove

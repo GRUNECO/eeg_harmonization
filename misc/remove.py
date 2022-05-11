d3072d18bdad981e879d0d6180fca026819d33e6
vhenaoi <vhenao.isaza@gmail.com>
2022-05-10 21:09:31 -0500

    remove

--- misc/remove.py
+++ misc/remove.py
@@ -1,8 +1,8 @@
 import os
 import glob
 
-name1 = ['_eeg','_stats']
-name2 = ['[restEC]']
+name = ['_eeg','_stats']
+#name2 = ['[restEC]']
 ext = ['.fif','.json','.txt']
 path = None
 #path = r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony'
@@ -11,10 +11,10 @@ path = None
 #path = r'D:\WEB\backend\filesSaved\SRM\derivatives\sovaharmony'
 def remove_data(path,sub,v,name,ext):
 #def remove_data(path,sub,name,ext):
-    path_def = path+'\\'+sub+'\\'+v+'\\eeg\\*'+name2+name1+ext
-    path_d = path+'\\'+sub+'\\'+v+'\\eeg\\*'+name1+ext
+    #path_def = path+'\\'+sub+'\\'+v+'\\eeg\\*'+name2+name1[0]+ext[0]
+    path_def = path+'\\'+sub+'\\'+v+'\\eeg\\*'+name[0]+ext[0]
     #path_def = path+'\\'+sub+'\\eeg\\*'+name+ext
-    if name2 in path_def:
+    if name in path_def:
         icpowers_json_files = glob.glob(path_d)
         #icpowers_json_files = glob.glob(path_def)
         json_remove.append(icpowers_json_files)
@@ -35,6 +35,6 @@ for i in json_remove:
      for j in range(0,2):
          try:
              print(i[j])
-             os.remove(i[j])
+             #os.remove(i[j])
          except:
           continue 

c53c90b874da5b1eeccf3242cf8f4f9217b4ab65
vhenaoi <vhenao.isaza@gmail.com>
2022-05-21 16:22:53 -0500

    inteto lemon

--- preprocessingEEG.py
+++ preprocessingEEG.py
@@ -1,19 +1,19 @@
 from sovaharmony.processing import harmonize 
 from sovaharmony.getDataframes import get_dataframe_prep,get_dataframe_wica,get_dataframe_powers,get_dataframe_reject
-from datasets import SRM,BIOMARCADORES_test, BIOMARCADORES
+from datasets import LEMON
 
 
-THE_DATASETS=[BIOMARCADORES_test,SRM]
+THE_DATASETS=[LEMON]
 
 for dataset in THE_DATASETS:
-    #process=harmonize(dataset,fast_mode=False)
+    process=harmonize(dataset,fast_mode=False)
     # get_dataframe_powers(dataset,mode="channels",stage=None)
     # get_dataframe_powers(dataset,mode="channels",stage="norm")
     # get_dataframe_powers(dataset,mode="components",stage=None)
     # get_dataframe_powers(dataset,mode="components",stage="norm")
     # get_dataframe_prep(dataset)
     # get_dataframe_wica(dataset)
-    get_dataframe_reject(dataset)
+    #get_dataframe_reject(dataset)
 
 
 

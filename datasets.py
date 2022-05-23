7ec5cbc16f329826818366d2d2b053587bb20c59
SERVIDOR\Luisa <luisa.zapatas@udea.edu.co>
2022-05-23 09:09:07 -0500

    Elimino dataset.py de sovaharmony

--- datasets.py
+++ datasets.py
@@ -1,13 +1,24 @@
 BIOMARCADORES = {
     'name':'BIOMARCADORES',
+    #'input_path':r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS',
     'input_path':r'F:\BIOMARCADORES',
+    'layout':{'extension':'.vhdr', 'task':'CE','suffix':'eeg', 'return_type':'filename'},
+    'args':{'line_freqs':[60]},
+    'group_regex':'(.+).{3}',
+    'events_to_keep':None,
+    'run-label':'restCE',
+    'session':'V'
+}
+
+BIOMARCADORESMini = {
+    'name':'BIOMARCADORES',
+    'input_path':r'E:\Academico\Universidad\Posgrado\Tesis\CodificadoBIDSMini',
     'layout':{'extension':'.vhdr', 'task':'CE','suffix':'eeg', 'return_type':'filename'},
     'args':{'line_freqs':[60]},
     'group_regex':'(.+).{3}',
     'events_to_keep':None,
     'run-label':'restCE'
 }
-
 
 BIOMARCADORES_test = {
     'name':'BIOMARCADORES',
@@ -16,7 +27,42 @@ BIOMARCADORES_test = {
     'args':{'line_freqs':[60]},
     'group_regex':'(.+).{3}',
     'events_to_keep':None,
-    'run-label':'restCE'
+    'run-label':'restCE',
+    'session':'V'
+}
+CHBMP = {
+    'name':'CHBMP',
+    'input_path':r'D:\BASESDEDATOS\CHBMP',
+    'layout':{'extension':'.edf', 'task':'protmap','suffix':'eeg', 'return_type':'filename'},
+    'args':{'line_freqs':[60],},
+    'group_regex':None,
+    'events_to_keep':[65],
+    'run-label':'restCE',
+    'session':None
+}
+
+LEMON = {
+    'name':'LEMON',
+    'input_path':r'D:\BASESDEDATOS\LEMON_BIDS',
+    'layout':{'extension':'.vhdr', 'task':'resting','suffix':'eeg', 'return_type':'filename'},
+    'args':{'resample':1000,'line_freqs':[50],},
+    'group_regex':None,
+    'events_to_keep':[5],
+    'run-label':'restEC',
+    'session':None
+}
+
+
+
+BIOMARCADORES_test = {
+    'name':'BIOMARCADORES',
+    'input_path':r'D:\WEB\backend\filesSaved\PRUEBA',
+    'layout':{'extension':'.vhdr', 'task':'OE','suffix':'eeg', 'return_type':'filename'},
+    'args':{'line_freqs':[60]},
+    'group_regex':'(.+).{3}',
+    'events_to_keep':None,
+    'run-label':'restCE',
+    'session':'V'
 }
 
 SRM_test = {
@@ -26,34 +72,18 @@ SRM_test = {
     'args':{'line_freqs':[50]},
     'group_regex':None,
     'events_to_keep':None,
-    'run-label':'restCE'
+    'run-label':'restCE',
+    'session':'V'
 }
 
-SRM= {
+SRM = {
     'name':'SRM',
     'input_path':r'D:\WEB\backend\filesSaved\SRM',
     'layout':{'extension':'.edf', 'task':'resteyesc','suffix':'eeg', 'return_type':'filename'},
     'args':{'line_freqs':[50]},
     'group_regex':None,
     'events_to_keep':None,
-    'run-label':'restCE'
-}
-CHBMP = {
-    'name':'CHBMP',
-    'input_path':r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\CHBMP',
-    'layout':{'extension':'.edf', 'task':'protmap','suffix':'eeg', 'return_type':'filename'},
-    'args':{'line_freqs':[60],},
-    'group_regex':None,
-    'events_to_keep':[65],
-    'run-label':'restCE'
+    'run-label':'restCE',
+    'session':'V'
 }
 
-LEMON = {
-    'name':'LEMON',
-    'input_path':r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\LEMON_BIDS',
-    'layout':{'extension':'.vhdr', 'task':'resting','suffix':'eeg', 'return_type':'filename'},
-    'args':{'resample':1000,'line_freqs':[50],},
-    'group_regex':None,
-    'events_to_keep':[5],
-    'run-label':'restCE'
-}

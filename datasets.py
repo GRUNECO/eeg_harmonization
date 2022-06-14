0e73c859a378637ef86a030b07b15b6cde5cd807
vhenaoi <vhenao.isaza@gmail.com>
2022-06-13 22:29:56 -0500

    actualizar

--- datasets.py
+++ datasets.py
@@ -1,7 +1,7 @@
 BIOMARCADORES = {
     'name':'BIOMARCADORES',
-    #'input_path':r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS',
-    'input_path':r'F:\BIOMARCADORES',
+    'input_path':r'E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS',
+    #'input_path':r'F:\BIOMARCADORES',
     'layout':{'extension':'.vhdr', 'task':'CE','suffix':'eeg', 'return_type':'filename'},
     'args':{'line_freqs':[60]},
     'group_regex':'(.+).{3}',

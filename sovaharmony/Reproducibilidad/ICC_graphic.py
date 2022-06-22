2b4d7ececde88a507659c4f1a4dd7a8883481aac
vhenaoi <vhenao.isaza@gmail.com>
2022-06-22 13:44:37 -0500

    Imagenes ICC con valores

--- sovaharmony/Reproducibilidad/ICC_graphic.py
+++ sovaharmony/Reproducibilidad/ICC_graphic.py
@@ -2,15 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns 
-icc_data_Roi=pd.read_csv(r'sovaharmony\Reproducibilidad\ICC_values_csv\icc_values_ROIS_G2-CTR.csv',sep=';')
-icc_data_Comp=pd.read_csv(r'sovaharmony\Reproducibilidad\ICC_values_csv\icc_values_Components_G2-CTR.csv',sep=';')
+icc_data_Roi=pd.read_csv(r'eeg_harmonization\sovaharmony\Reproducibilidad\ICC_values_csv\icc_values_ROIS_G2-CTR.csv',sep=';')
+icc_data_Comp=pd.read_csv(r'eeg_harmonization\sovaharmony\Reproducibilidad\ICC_values_csv\icc_values_Components_G2-CTR.csv',sep=';')
 
 
 def barplot_icc_nB_1G(icc_data,x_value,group,plot=False,save=False):
     filter_band=icc_data[icc_data['Group']==group]
     sns.set(font_scale = 0.9)
     sns.set_theme(style="white")
-    ax=sns.catplot(x=x_value,y='ICC',data=filter_band,hue='Stage',palette='winter_r',kind='bar',col='Bands',col_wrap=4,legend=False)
+    ax=sns.catplot(x=x_value,y='ICC',data=filter_band,hue='Stage',palette='winter_r',kind='bar',col='Bands',col_wrap=4,legend=False, ci=True, capsize=.2)
+     #ax.despine(left=True)
     ax.fig.suptitle('ICC3k for '+ x_value +' in frequency bands of '+group+' group')
     ax.add_legend(loc='upper center',bbox_to_anchor=(.5,0.94),ncol=2)
     ax.fig.subplots_adjust(top=0.829,bottom=0.133, right=0.936,left=0.062, hspace=0.143, wspace=0.11) # adjust the Figure in rp
@@ -18,8 +19,16 @@ def barplot_icc_nB_1G(icc_data,x_value,group,plot=False,save=False):
     ax.set(ylabel=None)
     ax.fig.text(0.5, 0.07, x_value, ha='center', va='center')
     ax.fig.text(0.03, 0.5,  'ICC', ha='center', va='center',rotation='vertical')
+    # iterate through axes
+    for axs in ax.axes.ravel():
+        # add annotations
+        for c in axs.containers:
+            labels = [f'{(v.get_height()):.3f}' for v in c]
+            #axs.bar_label(c, labels=labels, label_type='center',size=7,rotation='vertical',color='w')
+            axs.bar_label(c, labels=labels, label_type='edge',size=5,rotation='vertical')
+        #axs.margins(y=0.02)
     if save==True:
-        plt.savefig('sovaharmony\Reproducibilidad\ICC_Graphics\ICC_{name_group}_{tipo}.png'.format(name_group=group,tipo=x_value))
+        plt.savefig(r'eeg_harmonization\sovaharmony\Reproducibilidad\ICC_Graphics\ICC_{name_group}_{tipo}.png'.format(name_group=group,tipo=x_value))
         plt.close()
     if plot:
         plt.show()
@@ -27,7 +36,8 @@ def barplot_icc_nB_1G(icc_data,x_value,group,plot=False,save=False):
 def barplot_icc_comp_nG(icc_data,x_value,plot=False,save=False):
     sns.set(font_scale = 0.9)
     sns.set_theme(style="white")
-    ax=sns.catplot(x=x_value,y='ICC',data=icc_data,hue='Group',palette='winter_r',kind='bar',col='Bands',col_wrap=4,legend=False)
+    ax=sns.catplot(x=x_value,y='ICC',data=icc_data,hue='Group',palette='winter_r',kind='bar',col='Bands',col_wrap=4,legend=False,ci=True, capsize=.2)
+    #ax.despine(left=True)
     ax.fig.suptitle('ICC3k for '+ x_value +' in frequency bands')
     ax.add_legend(loc='upper center',bbox_to_anchor=(.5,0.94),ncol=2)
     ax.fig.subplots_adjust(top=0.829,bottom=0.133, right=0.936,left=0.062, hspace=0.143, wspace=0.11) # adjust the Figure in rp
@@ -35,20 +45,30 @@ def barplot_icc_comp_nG(icc_data,x_value,plot=False,save=False):
     ax.set(ylabel=None)
     ax.fig.text(0.5, 0.07, x_value, ha='center', va='center')
     ax.fig.text(0.03, 0.5,  'ICC', ha='center', va='center',rotation='vertical')
+    # iterate through axes
+    for axs in ax.axes.ravel():
+        # add annotations
+        for c in axs.containers:
+            labels = [f'{(v.get_height()):.3f}' for v in c]
+            #axs.bar_label(c, labels=labels, label_type='center',size=7,rotation='vertical', color='w')
+            axs.bar_label(c, labels=labels, label_type='edge',size=5,rotation='vertical')
+        #axs.margins(y=0.02)
     if save==True:
-        plt.savefig('sovaharmony\Reproducibilidad\ICC_Graphics\ICC_Comparacióngrupos_{tipo}.png'.format(tipo=x_value))
+        plt.savefig(r'eeg_harmonization\sovaharmony\Reproducibilidad\ICC_Graphics\ICC_Comparacióngrupos_{tipo}.png'.format(tipo=x_value))
         plt.close()
     if plot:
         plt.show()
 
-    
 
-barplot_icc_comp_nG(icc_data_Roi[icc_data_Roi['Stage']=='Normalized data'],'Roi',plot=True,save=True)
-barplot_icc_comp_nG(icc_data_Comp[icc_data_Comp['Stage']=='Normalized data'],'Components',plot=True,save=True)
-barplot_icc_nB_1G(icc_data_Roi,'Roi','G2',plot=True,save=True)
-barplot_icc_nB_1G(icc_data_Roi,'Roi','CTR',plot=True,save=True)
-barplot_icc_nB_1G(icc_data_Comp,'Components','G2',plot=True,save=True)
-barplot_icc_nB_1G(icc_data_Comp,'Components','CTR',plot=True,save=True)
+pl = True
+sa = False    
+
+barplot_icc_comp_nG(icc_data_Roi[icc_data_Roi['Stage']=='Normalized data'],'Roi',plot=pl,save=sa)
+barplot_icc_comp_nG(icc_data_Comp[icc_data_Comp['Stage']=='Normalized data'],'Components',plot=pl,save=sa)
+barplot_icc_nB_1G(icc_data_Roi,'Roi','G2',plot=pl,save=sa)
+barplot_icc_nB_1G(icc_data_Roi,'Roi','CTR',plot=pl,save=sa)
+barplot_icc_nB_1G(icc_data_Comp,'Components','G2',plot=pl,save=sa)
+barplot_icc_nB_1G(icc_data_Comp,'Components','CTR',plot=pl,save=sa)
 
 def icc_mean(data):
     Stage=data['Stage'].unique()

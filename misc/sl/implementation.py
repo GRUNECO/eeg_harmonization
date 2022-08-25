67a9b47dd1a5a41b6b041a48a2927680250168f9
vhenaoi <vhenao.isaza@gmail.com>
2022-08-25 13:23:28 +0200

    continuo y por epocas

--- misc/sl/implementation.py
+++ misc/sl/implementation.py
@@ -3,24 +3,47 @@
 
 """
 
+from lib2to3.pgen2.token import LSQB
 from time import time
-from load_cnt import load
+from load_cnt import load_epoch, load_continuos
 from sl import get_sl
 from matplotlib import pyplot as plt
+import numpy as np
 
-
+path = r"E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony\sub-CTR001\ses-V0\eeg\sub-CTR001_ses-V0_task-CE_desc-norm_eeg"
 #capture the start time
 star_time = time()
 #Read input data
-data, fs = load(r"E:\Academico\Universidad\Posgrado\Tesis\Datos\BASESDEDATOS\BIOMARCADORES_BIDS\derivatives\sovaharmony\sub-CTR001\ses-V0\eeg\sub-CTR001_ses-V0_task-OE_desc-norm_eeg")
-#For default values
+data, fs = load_continuos(path)
 sl = get_sl(data, fs)
 #For parameter control
 #sl = get_sl(data, fs, time_delay=4, w1=16, w2=215, pref=0.05)
-
+M1 =100*sl
+M1[np.diag_indices_from(M1)] = 1
 #Show sl data
-print(sl)
-
+print(M1)
+plt.pcolor(M1,cmap=plt.cm.Blues)
+plt.ylabel('Canales')
+plt.xlabel('Canales')
+plt.show()
 #show the execution time
 print("The execution time [seconds]:")
 print(time()-star_time)
+
+data, fs = load_epoch(path)
+#For default values
+for raw in data:
+    sl = get_sl(raw._data, fs)
+    #For parameter control
+    #sl = get_sl(data, fs, time_delay=4, w1=16, w2=215, pref=0.05)
+    M1 =100*sl
+    M1[np.diag_indices_from(M1)] = 1
+    #Show sl data
+    print(M1)
+    plt.pcolor(M1,cmap=plt.cm.Blues)
+    plt.ylabel('Canales')
+    plt.xlabel('Canales')
+    plt.show()
+    #show the execution time
+    print("The execution time [seconds]:")
+    print(time()-star_time)
--- misc/sl/load_cnt.py
+++ misc/sl/load_cnt.py
@@ -7,7 +7,20 @@ import mne
 from sovaflow.utils import createRaw
 import numpy as np
 
-def load(path):
+def load_epoch(path):
+    try:
+        raw_data = mne.read_epochs(path + '.fif', verbose='error')
+        data = raw_data.get_data()
+        epoch_raw = []
+        for epoch in range(len(data)):
+            signal_epoch = data[epoch]
+            signal = createRaw(signal_epoch,raw_data.info['sfreq'])
+            epoch_raw.append(signal)
+        return epoch_raw, raw_data.info['sfreq']
+    except:
+        print("The file path: %s no found"%path)
+
+def load_continuos(path):
     try:
         raw_data = mne.read_epochs(path + '.fif', verbose='error')
         data = raw_data.get_data()

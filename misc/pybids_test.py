712dcd41731e9b54507d5f61db2cbd9859639fef
vhenaoi <vhenao.isaza@gmail.com>
2022-10-24 22:35:39 +0200

    mensaje test bids

--- misc/pybids_test.py
+++ misc/pybids_test.py
@@ -1,6 +1,11 @@
 from bids import BIDSLayout
+import os
 
-data_path = r'E:\CodificadoBIDS'
-layout = BIDSLayout(data_path)
-eegs = layout.get(extension='.vhdr', task='CE',suffix='eeg', return_type='filename')
-eegs += layout.get(extension='.vhdr', task='OE',suffix='eeg', return_type='filename')
+data_path = r'E:\Academico\Universidad\Posgrado\Tesis\Datos\OTRASBASESDEDATOS\NEW'
+try:
+    layout = BIDSLayout(data_path)
+    eegs = layout.get(extension='.vhdr', task='CE',suffix='eeg', return_type='filename')
+    eegs += layout.get(extension='.vhdr', task='OE',suffix='eeg', return_type='filename')
+    print('BIDS format OK')
+except:
+    print('Try to convert of Dataset in BIDS format using "conversion_bids"')

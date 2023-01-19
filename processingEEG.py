8275fa703c93278ef0acb68adb6a459751da7aad
vhenaoi <vhenao.isaza@gmail.com>
2023-01-19 14:19:49 -0500

    ICC crudos

--- processingEEG.py
+++ processingEEG.py
@@ -1,20 +1,20 @@
 from sovaharmony.preprocessing import harmonize 
-from sovaharmony.postprocessing import features
+from sovaharmony.postprocessingprep import features
 from sovaharmony.data_structure.getDataframes import get_dataframe_prep
 from sovaharmony.data_structure.getDataframes import get_dataframe_wica
 from sovaharmony.data_structure.getDataframes import get_dataframe_reject
 from sovaharmony.data_structure.query_derivatives import get_dataframe_columnsIC
 from sovaharmony.data_structure.query_derivatives import get_dataframe_columnsROI
-from sovaharmony.datasets import CHBMP as DATA    
+from sovaharmony.datasets import BIOMARCADORES_CE as DATA    
 import time 
 
 THE_DATASETS=[DATA]
 for dataset in THE_DATASETS:
     # Preprocessing pipeline
-    start = time.perf_counter()
-    process=harmonize(dataset,fast_mode=False)
-    final = time.perf_counter()
-    print('TIME PREPROCESSING:::::::::::::::::::', final-start)
+    #start = time.perf_counter()
+    #process=harmonize(dataset,fast_mode=False)
+    #final = time.perf_counter()
+    #print('TIME PREPROCESSING:::::::::::::::::::', final-start)
 
     # Postprocessing pipeline (extraction of features)
     start = time.perf_counter()
@@ -23,16 +23,16 @@ for dataset in THE_DATASETS:
     print('TIME POSTPROCESSING:::::::::::::::::::', final-start)
 
     # Preprocessing files 
-    start = time.perf_counter()
-    get_dataframe_prep(dataset)
-    get_dataframe_wica(dataset)
-    get_dataframe_reject(dataset)
-    metricas=['power','sl','crossfreq','entropy','cohfreq']
-    for i in metricas:
-        get_dataframe_columnsIC(dataset,feature=i)
-        get_dataframe_columnsROI(dataset,feature=i)
-    final = time.perf_counter()
-    print('TIME DERIVATIVES OF PREPROCESSING:::::::::::::::::::', final-start)
+    #start = time.perf_counter()
+    #get_dataframe_prep(dataset)
+    #get_dataframe_wica(dataset)
+    #get_dataframe_reject(dataset)
+    #metricas=['power','sl','crossfreq','entropy','cohfreq']
+    #for i in metricas:
+    #    get_dataframe_columnsIC(dataset,feature=i)
+    #    get_dataframe_columnsROI(dataset,feature=i)
+    #final = time.perf_counter()
+    #print('TIME DERIVATIVES OF PREPROCESSING:::::::::::::::::::', final-start)
 
 
     
